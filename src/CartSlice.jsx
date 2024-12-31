2e76bf59fe6a582fe7ea318a802154964b3ef49e
Huy Hoang Hoang <u7671528@anu.edu.au>
2024-12-31 16:55:07 +1100

    Complete  basic features
    -Display products
    -Add/remove items from cart

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,23 +1,31 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const CartSlice = createSlice({
-  name: 'cart',
-  initialState: {
-    items: [], // Initialize items as an empty array
-  },
-  reducers: {
-    addItem: (state, action) => {
-    
+    name: 'cart',
+    initialState: {
+        items: [], // Initialize items as an empty array
     },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
+    reducers: {
+        addItem: (state, action) => {
+            const addedItem = action.payload;
+            const existingItem = state.items.find(item => item.name === addedItem.name);
 
-    
+            if(existingItem)
+                existingItem.quantity++;
+            else
+                state.items.push({...addedItem, quantity: 1});
+        },
+
+        removeItem: (state, action) => {
+            state.items = state.items.filter(item => item.name !== action.payload.name);
+        },
+
+        updateQuantity: (state, action) => {
+            const itemToUpdate = state.items.find(item => item.name === action.payload.name);
+            itemToUpdate.quantity = action.payload.quantity;
+        },
     },
-  },
 });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
-
 export default CartSlice.reducer;
